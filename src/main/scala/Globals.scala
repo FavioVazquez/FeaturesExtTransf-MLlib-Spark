a1af925630135a01976d1463fb235ed7e8b9a4df
FavioVazquez <favio.vazquezp@gmail.com>
2015-05-18 16:00:25 -0430

    - Fix globals. Now using packages

--- src/main/scala/Globals.scala
+++ src/main/scala/Globals.scala
@@ -1,8 +1,11 @@
 /**
  * Created by Favio on 18/05/15.
  */
+
+package globals{
 object Globals {
 
   val masterSpark = "mesos://master.mcbo.mood.com.ve:5050"
   val masterHDFS = "hdfs://master.mcbo.mood.com.ve:8020/"
+  }
 }
